52af188f7126ca551f2e0fbbc6fce6a6f6184ea8
Hem Raj Bhusal <razzh49@gmail.com>
2023-07-03 10:36:53 -0400

    initial commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,6 +5,15 @@ import { AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
 import { Icon, Button } from "@chakra-ui/react";
 
 function Login(props) {
+  const bgImage = {
+    imageUrl: "./images/bg.jpeg",
+    beds: 3,
+    baths: 2,
+    title: "Modern home in city center in the heart of historic Los Angeles",
+    formattedPrice: "$1,900.00",
+    reviewCount: 34,
+    rating: 4,
+  };
   return (
     <div>
       <h2>EVISTRA lOGIN</h2>
